8b3d91fb2a82bccf9433339a9914678a478cd22c
amaduse <amaduse@gmail.com>
2024-03-24 00:47:16 -0300

    Add files via upload

---
+++ primeiro/primeiro.go
@@ -0,0 +1,20 @@
+// Programas executáveis iniciam com pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+
+import "fmt"
+
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
+
+/*
+1) Priorize códigos limpos e faça comentários que agregam valor!
+2) Evite comentários óbvios
+3) Durante o curso abuse dos comentários
+*/
